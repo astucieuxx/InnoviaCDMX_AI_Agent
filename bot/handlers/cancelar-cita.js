f4df8e7e4c576d4c769625b5d8e05c9ea59dcdd7
Benjamin Miranda <ben.miranda1@gmail.com>
2026-02-22 17:58:48 -0600

    Corregir error de sintaxis en cancelar-cita.js - indentación incorrecta

--- bot/handlers/cancelar-cita.js
+++ bot/handlers/cancelar-cita.js
@@ -40,15 +40,16 @@ function formatDateSpanish(dateInput) {
         date = new Date(dateInput);
         
         // If that fails, try parsing as YYYY-MM-DD
-      if (isNaN(date.getTime()) && dateInput.match(/^\d{4}-\d{2}-\d{2}/)) {
-        const [year, month, day] = dateInput.split('-').map(Number);
-        date = new Date(year, month - 1, day);
-      }
-      
-      // If still fails, try DD/MM/YYYY
-      if (isNaN(date.getTime()) && dateInput.match(/^\d{1,2}\/\d{1,2}\/\d{4}/)) {
-        const [day, month, year] = dateInput.split('/').map(Number);
-        date = new Date(year, month - 1, day);
+        if (isNaN(date.getTime()) && dateInput.match(/^\d{4}-\d{2}-\d{2}/)) {
+          const [year, month, day] = dateInput.split('-').map(Number);
+          date = new Date(year, month - 1, day);
+        }
+        
+        // If still fails, try DD/MM/YYYY
+        if (isNaN(date.getTime()) && dateInput.match(/^\d{1,2}\/\d{1,2}\/\d{4}/)) {
+          const [day, month, year] = dateInput.split('/').map(Number);
+          date = new Date(year, month - 1, day);
+        }
       }
     } else {
       console.error('Invalid date input type:', typeof dateInput);
